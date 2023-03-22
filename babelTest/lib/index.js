eff47d06b3b24c847f90f3164e00dec84be08f5e
gautamgoshopmatic <gautam.fonseca@goshopmatic.com>
2023-03-22 17:48:36 +0530

    initial commit

---
+++ babelTest/lib/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var fruits = ['apples', 'oranges', 'mangos'];
+var fruitcopy = [].concat(fruits);
+
+var addition = function addition(a, b) {
+  return a + b;
+};
---
+++ babelTest/src/index.js
@@ -0,0 +1,5 @@
+const fruits = ['apples', 'oranges', 'mangos']
+
+const fruitcopy = [...fruits]
+
+const addition = (a,b) => a + b
